7ed18e2ab4ab2be7b70c86022dcf253e2a399916
Lucas <lucasding@foxmail.com>
2021-08-14 14:35:43 +0800

    成功运行

--- env.py
+++ env.py
@@ -7,8 +7,8 @@ class ChaseEnv(object):
     viewer = None
     dt = 0.1
     alpha_bound = [0, np.pi]
-    state_dim = 3
-    alpha_dim = 1
+    state_dim = 2
+    action_dim = 1
 
     def __init__(self, _R, _r, _sheepTheta, _sheepV, _dogTheta, _dogV):
         self.state = np.zeros(3, dtype=np.float32)
@@ -24,40 +24,52 @@ class ChaseEnv(object):
         self.sheepTheta = _sheepTheta
         self.dogTheta = _dogTheta
 
-    def step(self, alpha):
+    def step(self, action):
         done = False
-        r = self.r
+        r = self.state[0]
         R = self.R
-        theta = self.sheepTheta
+        theta = self.state[1]
         dt = self.dt
         v = self.sheepV
         V = self.dogV
-        r_ = r + v * np.sin(alpha) * dt
-        x = np.sqrt(r ** 2 + (v * dt) ** 2 + 2 * r * v * np.sin(alpha) * dt)
-        beta = np.arcsin(v * np.cos(alpha) * dt / x)
-        if alpha < np.pi / 2:
+        r_ = r + v * np.sin(action) * dt
+        x = np.sqrt(r ** 2 + (v * dt) ** 2 + 2 * r * v * np.sin(action) * dt)
+        beta = np.arcsin(v * np.cos(action) * dt / x)
+        if action < np.pi / 2 :
             theta -= beta
-        else:
+        else :
             theta += beta
         self.state[0] = r_
         self.state[1] = theta
-        if abs(self.state[2] - self.state[1]) <= V / R * dt :
+        self.state[1] -= int(self.state[1] / (2 * np.pi)) * (2 * np.pi)
+        theta = np.fabs(self.state[1] - self.state[2])
+        theta -= int(theta / (2 * np.pi)) * (2 * np.pi)
+        if theta <= V / R * dt :
             self.state[2] = self.state[1]
-        elif self.state[2] < self.state[1] :
+        elif self.state[2] - self.state[1] > np.pi :
             self.state[2] += V / R * dt
         else:
-            self.state[2] -= V / R * dt
+            self.state[2] += 2 * np.pi - V / R * dt
+        self.state[2] -= int(self.state[2] / (2 * np.pi)) * (2 * np.pi)
 
         if self.state[0] >= R and np.fabs(self.state[1] - self.state[2]) != 0 :
             done = True
 
-        return done, self.state
+        #k, a, b, c, d = 1, 1, 0, 1, 0
+        #reward = k * (a * theta + b) / (c * (self.R - r_) + d)
+        a, b, c = 1, 1, -1
+        reward = a * r_ + b * theta + c
+
+        #print(r_, theta)
+
+        return [self.state[0], theta], reward[0], done
 
     def reset(self):
         self.state[0] = self.r
         self.state[1] = self.sheepTheta
         self.state[2] = self.dogTheta
-        return self.state
+        if self.viewer is not None : del self.viewer
+        return [self.state[0], np.fabs(self.state[1] - self.state[2])]
 
     def render(self):
         if self.viewer is None:
@@ -76,7 +88,3 @@ class ChaseEnv(object):
         self.viewer.add_geom(circle)
         self.viewer.add_geom(sheep)
         self.viewer.render()
-
-
-if __name__ == "__main__":
-    pass
---
+++ main.py
@@ -0,0 +1,51 @@
+import numpy as np
+import gym
+import time
+from rl import DDPG
+from env import ChaseEnv
+
+#####################  hyper parameters  ####################
+EPISODES = 200
+EP_STEPS = 5000
+MEMORY_CAPACITY = 10000
+RENDER = True
+
+_R = 10
+_r = 0
+_sheepTheta = 0
+_dogTheta = 0
+_dogV = 1
+_sheepV = 0.8
+
+env = ChaseEnv(_R, _r, _sheepTheta, _sheepV, _dogTheta, _dogV)
+s_dim = env.state_dim
+a_dim = env.action_dim
+a_bound = env.alpha_bound
+a_low_bound = env.alpha_bound[0]
+a_high_bound = env.alpha_bound[1]
+
+ddpg = DDPG(a_dim, s_dim, a_bound)
+var = 3  # the controller of exploration which will decay during training process
+t1 = time.time()
+for i in range(EPISODES):
+    observation = env.reset()
+    ep_r = 0
+    for j in range(EP_STEPS):
+        #if RENDER : env.render()
+        # add explorative noise to action
+        action = ddpg.choose_action(observation)
+        action = np.clip(np.random.normal(action, var), a_low_bound, a_high_bound)
+        observation_, reward, done = env.step(action)
+        ddpg.store_transition(observation, action, reward, observation_)  # store the transition to memory
+
+        if ddpg.pointer > MEMORY_CAPACITY:
+            var *= 0.9995  # decay the exploration controller factor
+            ddpg.learn()
+
+        observation = observation_
+        ep_r += reward
+        if j == EP_STEPS - 1 or done :
+            print('Episode: ', i, ' Reward: %i' % (ep_r), 'Explore: %.2f' % var, 'done' if done else '----')
+            if ep_r > 5000 : RENDER = True
+            break
+print('Running time: ', time.time() - t1)
